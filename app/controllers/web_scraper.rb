f4793f49aa2d26e66c0baf46aeb1c7f75f488a03
Salar Antinori <salar@Salars-MacBook-Pro.local>
2017-09-18 00:21:26 -0400

    wip

--- app/controllers/web_scraper.rb
+++ app/controllers/web_scraper.rb
@@ -1,9 +1,17 @@
+# required gems
 require 'HTTParty'
 require 'Nokogiri'
 require 'JSON'
 require 'Pry'
 require 'csv'
 
-page = HTTParty.get('https://raleigh.craigslist.org/search/apa')
+# endpoint to scrape
+# page = HTTParty.get('https://raleigh.craigslist.org/search/apa')
+page = 'https://raleigh.craigslist.org/search/apa'
+
+# turns scrapge into nokogiri object
+parse_page = Nokogiri::HTML(page)
+
+listings_array = []
 
 Pry.start(binding)
